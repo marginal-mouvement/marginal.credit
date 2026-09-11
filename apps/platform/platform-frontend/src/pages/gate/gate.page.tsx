97070ac0edfc4a82464c12b29c43990a546f66b8
aurerere <bigaureliend@gmail.com>
2026-09-11 12:30:46 +0200

    Feature: citadium event callout

--- apps/platform/platform-frontend/src/pages/gate/gate.page.tsx
+++ apps/platform/platform-frontend/src/pages/gate/gate.page.tsx
@@ -1,6 +1,11 @@
-import { ArrowUpRightIcon } from "lucide-react";
+import { ArrowUpRightIcon, InfoIcon } from "lucide-react";
 import { Badge } from "@marginal.credit/ui/badge.tsx";
 import { Card } from "@marginal.credit/ui/card.tsx";
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@marginal.credit/ui/alert.tsx";
 
 import { Header } from "../../parts/header.tsx";
 import { useUser } from "../../modules/auth/useUser.ts";
@@ -13,6 +18,15 @@ export const GatePage = () => {
     <>
       <Header title={`Bienvenue, ${user.name}`} />
       <Content withHeader className="flex flex-col gap-4">
+        {!user.createdAt && (
+          <Alert className="border-blue-200 bg-blue-50 text-blue-900">
+            <InfoIcon />
+            <AlertTitle>Item offert !</AlertTitle>
+            <AlertDescription>
+              Rdv au Citadium haussman pour récupérer un item offert (1er étage)
+            </AlertDescription>
+          </Alert>
+        )}
         <div className="flex justify-center items-center flex-col gap-4 pt-16 pb-24">
           <Badge variant="secondary">Mon solde</Badge>
           <Card className="p-4">
